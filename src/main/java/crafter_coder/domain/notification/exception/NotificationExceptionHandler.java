aff5709122a5970d82a5b940dbc909b6f0fe3ca2
ksah3756 <ksah3756@gmail.com>
2025-01-16 14:51:58 +0900

    fix: 알림 exceptionHandler에 로그 추가

--- src/main/java/crafter_coder/domain/notification/exception/NotificationExceptionHandler.java
+++ src/main/java/crafter_coder/domain/notification/exception/NotificationExceptionHandler.java
@@ -1,14 +1,17 @@
 package crafter_coder.domain.notification.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class NotificationExceptionHandler {
 
     @ExceptionHandler(NotificationException.class)
     public ResponseEntity<NotificationErrorCode> handleNotificationException(NotificationException e) {
+        log.error("NotificationException 발생: {}", e.getErrorCode().getMessage());
         return ResponseEntity.status(e.getErrorCode().getStatus()).body(e.getErrorCode());
     }
 }
