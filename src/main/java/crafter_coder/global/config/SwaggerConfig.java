49fbcdc9eafd9baaf416de5e51f57517883c0295
jieun <ju3391@naver.com>
2025-01-16 09:22:23 +0900

    fix/SwaggerConfig

--- src/main/java/crafter_coder/global/config/SwaggerConfig.java
+++ src/main/java/crafter_coder/global/config/SwaggerConfig.java
@@ -33,7 +33,7 @@ public class SwaggerConfig {
     private List<Server> servers() {
         List<Server> servers = new ArrayList<>();
         servers.add(new Server().url("http://localhost:8080").description("Local URL"));
-        servers.add(new Server().url(serverHost).description("Dev Server URL"));
+        //servers.add(new Server().url(serverHost).description("Dev Server URL"));
         return servers;
     }
 }
