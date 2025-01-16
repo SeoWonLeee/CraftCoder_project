52f3c134c4f25e67bbe1e2404895b523ca194331
ksah3756 <ksah3756@gmail.com>
2025-01-16 10:06:19 +0900

    fix: CustomAsyncExceptionHandler 로그 내용 변경, 주석 추가

--- src/main/java/crafter_coder/domain/notification/async/CustomAsyncExceptionHandler.java
+++ src/main/java/crafter_coder/domain/notification/async/CustomAsyncExceptionHandler.java
@@ -5,11 +5,14 @@ import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 
 import java.lang.reflect.Method;
 
+/*
+* 알림 발송 async 메소드가 void 반환하므로 예외처리 핸들러 추가 필요
+* */
 @Slf4j
 public class CustomAsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
     @Override
     public void handleUncaughtException(Throwable ex, Method method, Object... params) {
-        log.error("SSE 위한 스프링 이벤트 발행 중 예외 발생: {}", ex.getMessage());
+        log.error("클라이언트에게 알림 전송 중 예외 발생: {}", ex.getMessage());
         log.error("메소드 이름: {}", method.getName());
         for (Object param : params) {
             log.error("파라미터 값: {}", param);
