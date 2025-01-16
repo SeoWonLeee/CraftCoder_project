8645361e0f170ebbb3de5630bbb8818df13b5dfb
saehe park <119057831+popcifox@users.noreply.github.com>
2025-01-16 13:43:50 +0900

    refactory:UserRepository 수정
    user findbyIdandRole추가

--- src/main/java/crafter_coder/domain/user/repository/UserRepository.java
+++ src/main/java/crafter_coder/domain/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package crafter_coder.domain.user.repository;
 
+import crafter_coder.domain.user.model.Role;
 import crafter_coder.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User findByIdAndRole(Long id, Role role);
     Optional<User> findById(Integer id);
     Optional<User> findByUsername(String username);
 
