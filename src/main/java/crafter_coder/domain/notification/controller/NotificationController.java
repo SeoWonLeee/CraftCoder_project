cdb5798c22df870ab1c7f9149ee9732d20b76781
ksah3756 <ksah3756@gmail.com>
2025-01-16 11:19:10 +0900

    fix: NotificationController userId 일단 query parameter로 받도록 수정

--- src/main/java/crafter_coder/domain/notification/controller/NotificationController.java
+++ src/main/java/crafter_coder/domain/notification/controller/NotificationController.java
@@ -4,10 +4,7 @@ import crafter_coder.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
@@ -18,7 +15,7 @@ public class NotificationController {
 
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<SseEmitter> subscribe(
-            Long userId,
+            @RequestParam Long userId,
             @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId
     ) {
         return ResponseEntity.ok(notificationService.subscribe(userId, lastEventId));
