888f833e75dd6869271acc3eec2af9f9a0c92f8b
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-01-20 15:39:06 +0900

    ref: 사용자 ID로 관련 강좌 조회

---
+++ reservation-service/src/main/java/crafter_coder/domain/user_course/repository/UserCourseRepository.java
@@ -0,0 +1,10 @@
+package crafter_coder.domain.user_course.repository;
+
+import crafter_coder.domain.user_course.model.UserCourse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserCourseRepository extends JpaRepository<UserCourse, Long> {
+    List<UserCourse> findByUserId(Long userId);
+}
