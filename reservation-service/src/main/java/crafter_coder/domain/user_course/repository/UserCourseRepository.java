204b9986316c62a81bb55c19e6000e3647ce2225
jieun <ju3391@naver.com>
2025-01-21 23:56:13 +0900

    feat(UserCourseRepository): 상태 업데이트 추가

--- reservation-service/src/main/java/crafter_coder/domain/user_course/repository/UserCourseRepository.java
+++ reservation-service/src/main/java/crafter_coder/domain/user_course/repository/UserCourseRepository.java
@@ -3,12 +3,18 @@ package crafter_coder.domain.user_course.repository;
 import crafter_coder.domain.course.model.Course;
 import crafter_coder.domain.user.model.User;
 import crafter_coder.domain.user_course.model.UserCourse;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface UserCourseRepository extends JpaRepository<UserCourse, Long> {
+    // 특정 사용자가 신청한 모든 강좌 가져오기
     List<UserCourse> findByUserId(Long userId);
 
+    // 특정 강좌와 사용자 간의 UserCourse 존재 여부 확인
     boolean existsByCourseAndUser(Course course, User user);
+
+    // 특정 강좌와 사용자에 대한 UserCourse 가져오기 (상태 업데이트 등에서 사용)
+    Optional<UserCourse> findByCourseAndUser(Course course, User user);
 }
