e221eeaad8598c2e9e2936cf8dad140a7da44464
Kimseungin0529 <whffkaos007@naver.com>
2025-01-21 11:08:10 +0900

    refactor : 충돌 해결

--- reservation-service/src/main/java/crafter_coder/domain/user_course/repository/UserCourseRepository.java
+++ reservation-service/src/main/java/crafter_coder/domain/user_course/repository/UserCourseRepository.java
@@ -1,5 +1,7 @@
 package crafter_coder.domain.user_course.repository;
 
+import crafter_coder.domain.course.model.Course;
+import crafter_coder.domain.user.model.User;
 import crafter_coder.domain.user_course.model.UserCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +9,6 @@ import java.util.List;
 
 public interface UserCourseRepository extends JpaRepository<UserCourse, Long> {
     List<UserCourse> findByUserId(Long userId);
+
+    boolean existsByCourseAndUser(Course course, User user);
 }
