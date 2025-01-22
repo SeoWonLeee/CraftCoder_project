061394196a203b339a752b873c8ebae9046cc81c
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-01-22 22:21:06 +0900

    fix: UserRepository에 @Repository 애너테이션 추가

--- reservation-service/src/main/java/crafter_coder/domain/user/repository/UserRepository.java
+++ reservation-service/src/main/java/crafter_coder/domain/user/repository/UserRepository.java
@@ -3,9 +3,11 @@ package crafter_coder.domain.user.repository;
 import crafter_coder.domain.user.model.Role;
 import crafter_coder.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByIdAndRole(Long id, Role role);
     Optional<User> findByUsername(String username);
