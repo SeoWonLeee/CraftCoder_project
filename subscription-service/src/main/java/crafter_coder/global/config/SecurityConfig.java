63861ff5d94178f13f543aa737645fe7e7fffd8a
ksah3756 <ksah3756@gmail.com>
2025-01-22 11:40:01 +0900

    fix: subscription-service swagger 서버 포트 8081로 변경

--- subscription-service/src/main/java/crafter_coder/global/config/SecurityConfig.java
+++ subscription-service/src/main/java/crafter_coder/global/config/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
                 );
